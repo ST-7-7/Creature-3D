08628649a2643eb3eee80ed4a49755dcb06cfcff
Sitong <125363150+ST-7-7@users.noreply.github.com>
2024-07-24 08:17:53 +0930

    Add files via upload

---
+++ data/content.js
@@ -0,0 +1,31 @@
+const CONTENT_INFO = {
+  otter: {
+    h1: "Otter",
+    h2: "Explore the Cuteness and Playfulness of Sea Otters",
+    p: "Sea otters are adorable and agile marine mammals, renowned for their nimble swimming skills and unique foraging techniques."
+  },
+
+  cat: {
+    h1: "Cat",
+    h2: "Unveiling the Mysterious Lives of Cats",
+    p: "Cats are companionable pets known for their independence, graceful demeanor, and acute senses, making them beloved by many."
+  },
+
+  owl: {
+    h1: "Owl",
+    h2: "Embark on the Nightly Wonders of Owls",
+    p: "Owls, the guardians of the night, hunt with their keen hearing and vision, symbolizing wisdom and mystery."
+  },
+
+  cockatoo: {
+    h1: "Cockatoo",
+    h2: "Discovering their Delightful World",
+    p: "With their vibrant personalities and distinctive crests, cockatoo are beloved for their playful antics and remarkable intelligence. Dive into their world of charm and discover the joy they bring!"
+  },
+
+  seal: {
+    h1: "Seal",
+    h2: "Chase the Graceful Moves of Seals in the Sea",
+    p: "Seals, the spirits of the sea, play an essential role in marine ecosystems with their soft fur and agile movements, making them indispensable to the oceanic world."
+  },
+};
